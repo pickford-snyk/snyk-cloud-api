8a925ec0be0f21c56982945112629b2e12bb1c6e
Owen Pickford <owen.pickford@snyk.io>
2023-05-09 17:09:14 -0700

    Add count resources script

---
+++ count_resources.sh
@@ -0,0 +1,22 @@
+ #!/bin/bash
+
+ Help() {
+    # Display Help
+    echo "Pass a resources JSON file to this script in order to count resources."
+    echo
+    echo "Syntax: count_resources.sh [-h|--help|-f|--file]"
+    echo "options:"
+    echo "-h, --help     Print this help"
+    echo "-f, --file     Pass the filename"
+ }
+
+ FILE_PATH=""
+
+ # Confirm JQ is installed
+ jq_exit=$(jq --version| echo $?)
+ if [ "$jq_exit" != "0" ]; then
+   echo "JQ not found. JQ must be installed to use this."
+   exit 1
+ fi
+ 
+ echo "$FILE_PATH" | jq -r '.data[] | .attributes | .resource_type' | sort | uniq -c
